4f657e57315d904c880740865c7a0c99f6a88a3a
Hemanth R H <hemanth.ramachandra@enparadigm.com>
2020-10-04 11:48:32 +0530

    Updates

---
+++ precache-manifest.c25df14bdb625067686251233d9ea24c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8c421dc85bb5d2b669323a8ae881e8b",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "0641bc85f14e912a716e",
+    "url": "/monsters-rolodex/static/css/main.183f3e91.chunk.css"
+  },
+  {
+    "revision": "78adac64eaa07ff35dea",
+    "url": "/monsters-rolodex/static/js/2.356d906f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.356d906f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0641bc85f14e912a716e",
+    "url": "/monsters-rolodex/static/js/main.34826f76.chunk.js"
+  },
+  {
+    "revision": "f553da120d846b640e5e",
+    "url": "/monsters-rolodex/static/js/runtime-main.1dfd2aab.js"
+  }
+]);
